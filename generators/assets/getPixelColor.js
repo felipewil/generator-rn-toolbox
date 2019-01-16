68f168dadccfd49b31faef5ee1b244920c398999
Felipe Leite <felipe.leite.gv@gmail.com>
2019-01-16 17:09:31 -0200

    Executing crop command before calling identify to correctly use the latter command on a 1 pixel image.

--- generators/assets/getPixelColor.js
+++ generators/assets/getPixelColor.js
@@ -4,8 +4,12 @@ module.exports = (imagePath, x, y) =>
   new Promise((resolve, reject) =>
     gm(imagePath)
       .crop(x, y)
-      .identify('%[hex:s]', (error, imageMagickColor) => {
-        if (error) return reject(error);
-        return resolve(`#${imageMagickColor}`);
+      .toBuffer((err, buffer) => {
+      	gm(buffer)
+      	.identify('%[hex:s]', (error, imageMagickColor) => {
+	        if (error) return reject(error);
+	        console.log(imageMagickColor);
+	        return resolve(`#${imageMagickColor}`);
+	      })
       })
   );
